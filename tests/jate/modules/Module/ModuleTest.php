5121137b98d3a36ef65a8879da8a6791170f1087
Federico Berra <fede.berra@hotmail.it>
2017-09-02 11:11:16 +0200

    Added backward compatibility to travis

--- tests/jate/modules/Module/ModuleTest.php
+++ tests/jate/modules/Module/ModuleTest.php
@@ -1,6 +1,9 @@
 <?php
   declare( strict_types = 1 );
-
+  // backward compatibility
+  if (!class_exists('\PHPUnit\Framework\TestCase')) {
+    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+  }
   use PHPUnit\Framework\TestCase;
 
   final class ModuleTest extends TestCase {
